d4df30bc52882f3ba30081dd8438f2d3a0abdf5c
matheus.santana <matheus.santana@TIT-WKS-08>
2018-03-14 18:31:24 -0300

    t

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,16 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController,AlertController,Loading,ActionSheetController,Platform,LoadingController,ToastController } from 'ionic-angular';
-<<<<<<< HEAD
 import { File,FileEntry } from '@ionic-native/file';
 import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
 import { Camera, CameraOptions } from '@ionic-native/camera';
-=======
-import { BarcodeScanner } from '@ionic-native/barcode-scanner';
-import { File } from '@ionic-native/file';
-import { Transfer, TransferObject } from '@ionic-native/transfer';
-import { FilePath } from '@ionic-native/file-path';
-import { Camera } from '@ionic-native/camera';
->>>>>>> d7c035bf2041a998151c4e2b6eb28529441298d3
 import {ApiService} from '../../shared/http-service';
 declare var cordova: any;
 
@@ -41,7 +33,6 @@ export class HomePage
     private api:ApiService
 ) { }
 
-<<<<<<< HEAD
   public fromGallery()
   {
     this.camera.getPicture({
@@ -56,27 +47,6 @@ export class HomePage
       this.error = JSON.stringify(error);
     });
   }
-=======
- // public barcodeData;
-  
-  // scan()
-  // {
-  //   const options = 
-  //   {
-  //     preferFrontCamera: false, // iOS and Android
-  //     showFlipCameraButton: true, // iOS and Android
-  //     showTorchButton: true, // iOS and Android
-  //     torchOn: false, // Android, launch with the torch switched on (if available)
-  //     prompt: 'Place a barcode inside the scan area', // Android
-  //       // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-  //     resultDisplayDuration: 500,
-
-  //       // Android only (portrait|landscape), default unset so it rotates with the device
-  //     orientation: 'portrait',
-  //     disableAnimations: true, // iOS
-  //     disableSuccessBeep: false // iOS
-  //   };
->>>>>>> d7c035bf2041a998151c4e2b6eb28529441298d3
 
   public capturePhoto()
   {
@@ -94,31 +64,6 @@ export class HomePage
     });
   }
 
-  public takePicture(sourceType) 
-  {
-    let options: CameraOptions = 
-    {
-      quality: 100,
-      destinationType: this.camera.DestinationType.FILE_URI,
-      sourceType: sourceType,
-      allowEdit: true,
-      encodingType: this.camera.EncodingType.JPEG,
-      targetWidth: 500,
-      targetHeight: 500,
-      saveToPhotoAlbum: false
-    };
- 
- 
-    this.camera.getPicture(options).then((img) => 
-    {
-      this.imageURI = img;
-      this.uploadPhoto(img);
-    }).catch((reason) => {
-      console.log(reason);
-    });
-
-  }
- 
 
   
   private presentToast(text) 
@@ -131,40 +76,6 @@ export class HomePage
     toast.present();
   }
    
-  
-  // doImageUpload() 
-  // {
-  //   let loader = this.loadingCtrl.create({
-  //     content: "Carre..."
-  //   });
-
-
-
-  //   let filename = this.imageURI.split('/').pop();
-  //   const fileTransfer: FileTransferObject = this.transfer.create();
- 
-  //   let options: FileUploadOptions = 
-  //   {
-  //     fileKey: "file",
-  //     fileName: filename,
-  //     chunkedMode: false,
-  //     mimeType: "image/jpg",
-  //     params: { 'title': "imageTitle"}
-  //   };
- 
-  //   loader.present();
-    
-  //  this.api.upload(this.imageURI).subscribe((res)=>
-  //   {
-  //     this.loading.dismissAll()
-  //     this.presentToast("sucesso");
-  //   },(err)=>
-  //   {
-  //     this.loading.dismissAll()
-  //     this.presentToast(`erro ${JSON.stringify(err)}`);
-  //   });
- 
-  // } 
 
   private uploadPhoto(imageFileUri: any): void 
   {
@@ -181,7 +92,6 @@ export class HomePage
   
   }
 
-<<<<<<< HEAD
   private readFile(file: any) 
   {
     const reader = new FileReader();
@@ -190,7 +100,9 @@ export class HomePage
     {
       const formData = new FormData();
       const imgBlob = new Blob([reader.result], {type: file.type});
-      formData.append('file', imgBlob, file.name);
+
+        formData.append('file', imgBlob, file.name);
+
         this.api.upload(formData).subscribe((res)=>
         {
           this.loading.dismissAll()
@@ -203,6 +115,4 @@ export class HomePage
     };
     reader.readAsArrayBuffer(file);
   }
-=======
->>>>>>> d7c035bf2041a998151c4e2b6eb28529441298d3
 }
